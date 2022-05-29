c3738e58585fea1b76c9d5b20c8bf9aaa3941e97
kliba <klibaner@mail.com>
2022-05-29 15:16:36 +0300

    atrem3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/header/Header'
-
-
+import StartPage2 from './components/StartPage2/StartPage2';
 import SecondPage2 from "./components/secondPage/SecondPage2";
 function App() {
   return (
