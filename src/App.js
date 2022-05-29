3e1821f49b74c6a539b25c8c20fc61060ba26ca1
kliba <klibaner@mail.com>
2022-05-29 15:22:22 +0300

    artem3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Header from './components/header/Header'
-import StartPage2 from './components/StartPage2/StartPage2';
-import SecondPage2 from "./components/secondPage/SecondPage2";
+
+import SmallItems from './components/StartPage2/smallIlems/SmallItems';
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <SecondPage2/>
+        <SmallItems/>
     </div>
   );
 }
