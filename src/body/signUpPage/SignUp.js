40c8976c6c4271ab1763d5d18f218f2d203da66d
Max <voronin223@gmail.com>
2022-06-21 17:59:00 +0300

    my changes

--- src/body/signUpPage/SignUp.js
+++ src/body/signUpPage/SignUp.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import './SignUp.css'
-import pixels from "./images/pexels-mo-10035858.jpg"
+import pixels from "./images/pexels-mo-10035858(1).jpg"
 class ControlledFormComponent extends Component {
     render() {
         return (
+
             <div>
 
-                <img src={pixels} alt="Start Page" width="25%"/>
-
+                <div class="split left">
+                    <div class="centred">
+                <img src={pixels} alt="Start Page" width="90%"/>
+                    </div>
+                </div>
                 <br />
+
+                <div class="split right">
+                    <div class="centred">
                 <div className='form'>
                 <h3>Create your Account</h3>
                 <br />
@@ -30,12 +37,18 @@ class ControlledFormComponent extends Component {
                 <form>
                     <input type="text" placeholder="" />
                 </form>
+
                 </div>
+
                 <br />
                 <br />
                 <div>
+                    <p className="btn"><span></span>
                     <button className='buttonReadMore' style={{}}>Read More >></button>
                     <button className='buttonJoinNow' style={{}}>Join now</button>
+                    </p>
+                </div>
+                </div>
                 </div>
             </div>
 
