6c16fc8c820a821aff18d7cf819eef691b0da004
artemLaptop <z96@mail.com>
2022-07-01 12:16:56 +0300

    hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import background from './images/StartPage.png'
 export default function Home() {
     return (
         <div>
-            <img src={background} alt="image" />
+            <img src={background} alt="images2233" />
         </div>
 
     )
