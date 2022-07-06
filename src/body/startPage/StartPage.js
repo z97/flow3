36436412ba29ad6dab70ac291027c03badfaa2e5
kliba <klibaner@mail.com>
2022-07-06 14:15:07 +0300

    server

--- src/body/startPage/StartPage.js
+++ src/body/startPage/StartPage.js
@@ -8,7 +8,7 @@ export default function StartPage() {
     <div>
       <img src={start} alt="Start Page" width="100%" />
       <div className="top-left"><h1 className='headerH1'>ReDesign <span style={{ "color": "white" }}>precious</span> things!</h1></div>
-        <div className="top-right"><h1 className='headerH1'><a href="#"><span style={{ "color": "black"}}>TO THE SHOP >></span></a> </h1></div>
+        <div className="top-right"><h1 className='headerH1'><a href="#"><span style={{ "color": "black"}}>TO THE SHOP</span></a> </h1></div>
       <div style={{ position: "relative", width: "fit-content", bottom: "1900px", left: "250px"}}>
       <button className='buttonSignUp' style={{}}>Sign Up</button>
       </div>
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,43 +1,56 @@
 import { Outlet, Link } from "react-router-dom";
 import Header from "../header/Header";
+import { useState, useEffect } from "react";
 
 const Layout = () => {
+  const stringifyData = data => JSON.stringify(data, null, 2)
+  const initialData = stringifyData({ data: null })
+  const [data, setData] = useState(initialData)
+  const [type, setType] = useState('Gold_Rings')
+
+  useEffect(() => {
+    const fetchData = () => {
+      const uri = 'http://localhost:4000/items?type=' + type 
+      fetch(uri)
+        .then(res => res.json())
+        .then((res) => {
+          const { title, type, description } = res[0]
+          const dataVal = stringifyData({
+            title,
+            type,
+            description
+          })
+          setData(dataVal)
+        })
+    }
+
+    fetchData()
+  }, [type])
+
   return (
     <>
-      <nav>
         <Header/>
-          <div style={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            padding: "20px"
-          }}>
-            <li>
-              <Link to="/jewels/type/necklaces">Necklaces</Link>
-            </li>
-            <li>
-              <Link to="/jewels/type/pendats">Pendats</Link>
-            </li>
-            <li>
-              <Link to="/jewels/type/chains">Chains</Link>
-            </li>
-            <li>
-              <Link to="/jewels/type/goldRings">Gold Rings</Link>
-            </li>
-            <li>
-              <Link to="/jewels/type/mansRings">Mans`s Rings</Link>
-            </li>
-            <li>
-              <Link to="/jewels/type/kidsJewellery">Kid`s Jewellery</Link>
-            </li>
-            <li>
-              <Link to="/jewels/type/fashionRings">Fashion Rings</Link>
-            </li>
-          </div>
-      </nav>
-      <Outlet />
+      <p>Click the buttons below to fetch data</p>
+      <button
+        onClick={() => setType('Gold_Rings')}
+        style={{ outline: type === 'Gold_Rings' ? '1px solid' : 0 }}
+      >
+        Gold Rings
+      </button>
+      <button
+        onClick={() => setType('Earrings')}
+        style={{ outline: type === 'Earrings' ? '1px solid' : 0 }}
+      >
+        Earrings
+      </button>
+
+      <section>
+        <pre>{data}</pre>
+      </section>
     </>
-  );
+  )
 };
 
 export default Layout;
+
+
